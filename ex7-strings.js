b103ef069a0b2fb58bbad123199feda0c8cafc98
v1trovao <viitrovao@gmail.com>
2025-03-27 15:32:40 -0300

    Exercicio String - Variacao com objetos e arrays

--- ex7-strings.js
+++ ex7-strings.js
@@ -8,11 +8,63 @@ const estrutura = ["eu", "gosto", "de", "programar",
     "ela", "adora", "estudar"
 ];
 
-function organizarFrase(palavras) {
+// Definição da estrutura como um objeto classificador de palavras
+const estrutura2 = {
+    sujeito: ["eu", "tu", "ela", "ele", "nós", "vós", "eles"],
+    verbo: ["gosto", "adora", "odeia", "prefere", "deitar"],
+    complemento: ["de", "programar", "estudar", "viajar", "nele"]
 
-    // Ordenar as palavras de acordo com a estrutura
-    return palavras.sort((a, b) => estrutura.indexOf(a) - estrutura.indexOf(b)).join(" ");
 }
 
-const palavras = ["gosto", "programar", "eu", "de"];
+// Recebe uma palavra e retorna o tipo de acordo com a estrutura
+function identificarTipo(palavra) {
+    // Percorrendo cada tipo como array
+    for (let tipo in estrutura2) {
+        // Verifica se o tipo contêm a palavra
+        if (estrutura2[tipo].includes(palavra)) {
+            return tipo; // Retorna o tipo correspondente
+        }
+    }
+
+    return "desconhecido"; // Caso a palavra não esteja na estrutura
+    
+}
+
+function organizarFrase(palavras) {
+
+    // Objeto frase com a palavra ordenada
+    let frase = {
+        sujeito: "",
+        verbo: "",
+        complemento: []
+    };
+
+    // Percorrendo cada elemento da palavra
+    palavras.forEach(palavra => {
+
+        // Identificar o tipo da palavra
+        let tipo = identificarTipo(palavra);
+
+        // Se for sujeito, e a frase ainda não tiver sujeito, define para a frase
+        if (tipo === "sujeito" && !frase.sujeito) {
+            frase.sujeito = palavra;
+        }
+        
+        // Se for verbo, e a frase ainda não tiver verbo, define para o verbo
+        else if (tipo === "verbo" && !frase.verbo) {
+            frase.verbo = palavra;
+        }
+
+        // Se for um complemento, adiciona na lista de complementos da frase
+        else if (tipo === "complemento") {
+            frase.complemento.push(palavra);
+        }
+    });
+    
+    return `${frase.sujeito} ${frase.verbo} ${frase.complemento.join(" ")}`.trim();
+    // Ordenar as palavras de acordo com a estrutura
+    // return `palavras.sort((a, b) => estrutura.indexOf(a) - estrutura.indexOf(b)).join(" ");`
+}
+
+const palavras = ["de", "gosto", "estudar", "programar", "adora", "eu", "ela", "jadice"];
 console.log(organizarFrase(palavras));
