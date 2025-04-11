fb02300221a32254f5360f314cdfcafcdcc2cf56
v1trovao <viitrovao@gmail.com>
2025-04-11 14:42:45 -0300

    "Exercicio - expressões 2 "

--- ex8-expressoes.js
+++ ex8-expressoes.js
@@ -10,11 +10,12 @@ Ex1: Crie um objeto usuario com as seguintes propriedades:
     no formato: "Usuário Ana, 25 anos."
 */
 
+// Entrada de Dados
 let nome = window.prompt("Qual o seu nome?");
 let idade = window.prompt("Qual sua idade? ");
 let hobbie = window.prompt("Qual seu hobbie principal?");
 
-// Função que cria um novo objeto Usuário
+// Função construtor que cria um novo objeto Usuário
 function Usuario(nome, idade, hobbie) {
     this.nome = nome;
     this.idade = idade;
@@ -22,11 +23,15 @@ function Usuario(nome, idade, hobbie) {
     this.hobbies = [hobbie];
 }
 
+// Arrow function q retorna uma string com os dados de um usuário
 const exibirUsuario = (usuario) => {
     return `Usuário ${usuario.nome}, ${usuario.idade} anos, hobbies: ${usuario.hobbies[0]}`
 }
 
+// criando um novo objeto Usuário
 let usuario = new Usuario(nome, idade, hobbie);
+
+// Saída de dados pelo console do navegador
 console.log(exibirUsuario(usuario));
 
 
--- ex8.1-expressoes.js
+++ ex8.1-expressoes.js
@@ -11,3 +11,42 @@ Ex2: Crie uma função construtora
 
     Instancie duas tarefas com new e exiba suas propriedades.
 */
+
+window.alert("Registro de tarefas");
+
+let desc = window.prompt("Descrição da tarefa: ");
+let prio = window.prompt("Qual a prioridade (de 1 a 5)?");
+
+function Tarefa(descricao, prioridade) {
+    this.descricao = descricao;
+    this.prioridade = prioridade
+    this.concluida = false;
+}
+
+const exibirTarefa = (tarefa) => {
+    if (tarefa.concluida) 
+        return `-> ${tarefa.descricao}, Prioridade: ${tarefa.prioridade}, Concluída`
+    else
+        return `-> ${tarefa.descricao}, Prioridade: ${tarefa.prioridade}, Pendente`
+}
+
+const marcarConcluida = (tarefa) => {
+    if (!tarefa.concluida)
+        tarefa.concluida = true;
+    else 
+        tarefa.concluida = false;
+}
+
+let tarefa1 = new Tarefa(desc, prio);
+let tarefa2 = new Tarefa("Pedir ajuda", 3);
+
+console.log(exibirTarefa(tarefa1));
+
+marcarConcluida(tarefa2);
+
+console.log(exibirTarefa(tarefa2));
+
+
+
+
+
